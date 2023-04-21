c5709a14d7b6038795da41cdeefafb4319a62afa
Igor Felix <igor.felix@loggi.com>
2023-04-21 13:09:01 -0300

    Add validation and models

--- internal/domain/entity/chat.go
+++ internal/domain/entity/chat.go
@@ -1,5 +1,11 @@
 package entity
 
+import (
+	"errors"
+
+	"github.com/google/uuid"
+)
+
 type ChatConfig struct {
 	Model            *Model
 	Temperature      float32
@@ -20,4 +26,79 @@ type Chat struct {
 	Status               string
 	TokenUsage           int
 	Config               *ChatConfig
+}
+
+func NewChat(userID string, initialSystemMessage *Message, chatConfig *ChatConfig) (*Chat, error) {
+	chat := &Chat{
+		ID:                   uuid.New().String(),
+		UserID:               userID,
+		InitialSystemMessage: initialSystemMessage,
+		Status:               "active",
+		Config:               chatConfig,
+		TokenUsage:           0,
+	}
+	chat.AddMessage(initialSystemMessage)
+
+	if err := chat.Validate(); err != nil {
+		return nil, err
+	}
+	return chat, nil
+}
+
+func (c *Chat) Validate() error {
+	if c.UserID == "" {
+		return errors.New("user_id is empty")
+	}
+	if c.Status != "active" && c.Status != "ended" {
+		return errors.New("invalid status")
+	}
+	if c.Config.Temperature < 0.0 || c.Config.Temperature > 1.0 {
+		return errors.New("invalid temperature")
+	}
+	return nil
+}
+
+func (c *Chat) AddMessage(m *Message) error {
+	if c.Status == "ended" {
+		return errors.New("chat is ended. no more messages allowed")
+	}
+	for {
+		if c.Config.Model.GetMaxTokens() >= m.GetQtdTokens()+c.TokenUsage {
+			c.Messages = append(c.Messages, m)
+			c.RefreshTokenUsage()
+			break
+		}
+		c.ErasedMessages = append(c.ErasedMessages, c.Messages[0])
+		c.Messages = c.Messages[1:]
+		c.RefreshTokenUsage()
+	}
+	return nil
+}
+
+func (c *Chat) EraseMessage(m *Message) {
+	for i, msg := range c.Messages {
+		if msg.ID == m.ID {
+			c.ErasedMessages = append(c.ErasedMessages, msg)
+			c.Messages = append(c.Messages[:i], c.Messages[i+1:]...)
+		}
+	}
+}
+
+func (c *Chat) GetMessages() []*Message {
+	return c.Messages
+}
+
+func (c *Chat) CountMessages() int {
+	return len(c.Messages)
+}
+
+func (c *Chat) End() {
+	c.Status = "ended"
+}
+
+func (c *Chat) RefreshTokenUsage() {
+	c.TokenUsage = 0
+	for msg := range c.Messages {
+		c.TokenUsage += c.Messages[msg].GetQtdTokens()
+	}
 }
